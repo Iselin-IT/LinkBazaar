96c8bd076280b71aca39f1b3c48d6edd07033ccb
Wambo64 <wambo64io@gmail.com>
2023-04-17 21:17:57 +0200

    Added Emojis / added migros

--- main.js
+++ main.js
@@ -2,10 +2,17 @@ const stores = [
   {
     name: "Aldi",
     urlScheme: "https://www.aldi-now.ch/de/search?q=",
+    emoji: "🛒",
   },
   {
     name: "Coop",
     urlScheme: "https://www.coop.ch/de/search/?text=",
+    emoji: "🏪",
+  },
+  {
+    name: "Migros",
+    urlScheme: "https://www.migros.ch/de/search?query=",
+    emoji: "🛍️",
   },
 ];
 
@@ -23,16 +30,31 @@ function convertListToUrls() {
     if (item.trim() !== "") {
       const encodedItem = encodeURIComponent(item.trim());
 
+      const itemLabel = document.createElement("h3");
+      itemLabel.textContent = item.trim();
+      urlList.appendChild(itemLabel);
+
+      const itemUrls = [];
       stores.forEach((store) => {
         const url = `${store.urlScheme}${encodedItem}`;
+        itemUrls.push(url);
         urls.push(url);
         const button = document.createElement("button");
-        button.innerText = `Search ${store.name}: ${item.trim()}`;
+        button.innerText = store.emoji;
         button.onclick = function () {
           window.open(url, "_blank");
         };
         urlList.appendChild(button);
       });
+
+      const openAllButton = document.createElement("button");
+      openAllButton.innerText = "Open All";
+      openAllButton.onclick = function () {
+        itemUrls.forEach((url) => {
+          window.open(url, "_blank");
+        });
+      };
+      urlList.appendChild(openAllButton);
 
       const separator = document.createElement("hr");
       urlList.appendChild(separator);
@@ -47,3 +69,5 @@ function openAllUrls() {
     window.open(url);
   });
 }
+
+convertListToUrls();
