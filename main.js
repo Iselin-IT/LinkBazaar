6e2ddbb8059b3b32723e21e40a30b45e00893d08
Wambo64 <wambo64io@gmail.com>
2023-04-17 21:06:13 +0200

    Added Coop

--- main.js
+++ main.js
@@ -1,32 +1,49 @@
+const stores = [
+  {
+    name: "Aldi",
+    urlScheme: "https://www.aldi-now.ch/de/search?q=",
+  },
+  {
+    name: "Coop",
+    urlScheme: "https://www.coop.ch/de/search/?text=",
+  },
+];
+
 let urls = [];
 
 function convertListToUrls() {
-    const inputList = document.getElementById("inputList");
-    const items = inputList.value.split("\n");
-    const urlList = document.getElementById("urlList");
+  const inputList = document.getElementById("inputList");
+  const items = inputList.value.split("\n");
+  const urlList = document.getElementById("urlList");
 
-    urlList.innerHTML = "";
-    urls = [];
+  urlList.innerHTML = "";
+  urls = [];
 
-    items.forEach(item => {
-        if (item.trim() !== "") {
-            const encodedItem = encodeURIComponent(item.trim());
-            const url = `https://www.aldi-now.ch/de/search?q=${encodedItem}`;
-            urls.push(url);
-            const button = document.createElement("button");
-            button.innerText = `Search: ${item.trim()}`;
-            button.onclick = function () {
-                window.open(url, "_blank");
-            };
-            urlList.appendChild(button);
-        }
-    });
+  items.forEach((item) => {
+    if (item.trim() !== "") {
+      const encodedItem = encodeURIComponent(item.trim());
 
-    document.getElementById("openUrlsBtn").disabled = urls.length === 0;
+      stores.forEach((store) => {
+        const url = `${store.urlScheme}${encodedItem}`;
+        urls.push(url);
+        const button = document.createElement("button");
+        button.innerText = `Search ${store.name}: ${item.trim()}`;
+        button.onclick = function () {
+          window.open(url, "_blank");
+        };
+        urlList.appendChild(button);
+      });
+
+      const separator = document.createElement("hr");
+      urlList.appendChild(separator);
+    }
+  });
+
+  document.getElementById("openUrlsBtn").disabled = urls.length === 0;
 }
 
 function openAllUrls() {
-    urls.forEach(url => {
-        window.open(url);
-    });
+  urls.forEach((url) => {
+    window.open(url);
+  });
 }
